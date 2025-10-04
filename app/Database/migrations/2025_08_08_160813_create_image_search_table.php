78540ab64246a71a380a251bf648888595a2b668
AmirMahdi-for <amirmahdifor@gmail.com>
2025-10-04 14:33:12 +0330

    fix: migration file

--- app/Database/migrations/2025_08_08_160813_create_image_search_table.php
+++ app/Database/migrations/2025_08_08_160813_create_image_search_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('try_on');
+        Schema::dropIfExists('image_search');
     }
 };
